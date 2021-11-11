528a1c42e241ce52b5000979ffdcfb537e06770b
Grace0212 <93570387+Grace0212@users.noreply.github.com>
2021-11-11 18:57:19 +0800

    Add files via upload

---
+++ Assignment1/src/student/StudentView.java
@@ -0,0 +1,10 @@
+package student;
+
+public class StudentView {
+	public void printStudent(int id, String name, String mathMark, String scienceMark) {
+		System.out.println("id: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Math Mark: " + mathMark);
+        System.out.println("Science Mark: " + scienceMark);
+	}
+}
